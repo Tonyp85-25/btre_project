f69c543a1a396698f3126291b577a32769ff4947
Tonyp85-25 <anthony-paulin@etudiant.oclock.io>
2020-08-30 18:31:48 +0200

    adding theme ,managing static files and 2more apps created

--- btre_project/settings.py
+++ btre_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,4 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'btre_project/static'
+]
