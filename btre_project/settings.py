eb9bc549387802ead2bde70378a94ff00279ce00
Tonyp85-25 <anthony-paulin@etudiant.oclock.io>
2020-09-05 17:49:21 +0200

    constants managed

--- btre_project/settings.py
+++ btre_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rmpu(_rpye_q0&)z3gcqq%lsi-q^kmuxn$6#5!_fafr3j_mswg'
+SECRET_KEY = MY_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
