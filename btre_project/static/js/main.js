0d641d09c41154f96e3b4491a8e7b634f1ed759c
Tonyp85-25 <anthony-paulin@etudiant.oclock.io>
2020-09-29 22:51:42 +0200

    js management of following WIP

--- btre_project/static/js/main.js
+++ btre_project/static/js/main.js
@@ -1,2 +1,27 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
+
+$('#follow').on('click', function(){
+    addToFav();
+});
+
+function addToFav() {
+    let userId = $('input[name="user_id"]').val();
+    let listingId = $('input[name="listing_id"]').val();
+    let listing = $('input[name="listing"]').val();
+
+    $.ajax({
+        url: '/followings/follow',
+        dataType: 'json',
+        method: 'POST',
+        data: {user_id: userId, listing_id:listingId, listing: listing},
+        success: function(obj){
+            console.log('success');
+            console.log (obj);
+        },
+        error: function(xHR,status, error){
+            console.log(xHR,status,error)
+        }
+
+    });
+}
